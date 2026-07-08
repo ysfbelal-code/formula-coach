deb4cf479e49324b163c488c044d1c6e14c1c789
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-07-08 14:43:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-import threading
 import streamlit as st
-from f1_23_data import start_f123_lap_telemetry, plot_telemetry, get_udp_data
+from f1_23_data import TelemetrySession, plot_telemetry, ip_address
 
 st.set_page_config("Formula Coach")
 st.title("Formula Coach")
-udp_details = get_udp_data()
-ip_address = udp_details[0]
-port = udp_details[1]
-
-text = f"Before you start, go to Settings -> Telemetry Settings, and insert the following values: \nUDP IP Address: {ip_address}\nUDP Port: {port}\nKeep in mind that NO data, for any reason whatsoever, is stored in our files. This is so that the UDP telemetry can send the input data to the program for it to work."
+text = f"Before you start, go to Settings -> Telemetry Settings, and insert the following values: \nUDP IP Address: {ip_address}\nUDP Port: 27000\nKeep in mind that NO data, for any reason whatsoever, is stored in our files. This is so that the UDP telemetry can send the input data to the program for it to work."
 
 st.text(text)
-if "stop_event" not in st.session_state:
-    st.session_state.stop_event = threading.Event()
+
+if "running" not in st.session_state:
     st.session_state.running = False
-    st.session_state.data = None
 
 col1, col2 = st.columns(2)
+
 with col1:
-    if st.button("Start"):
-        st.session_state.stop_event.clear()
+    if st.button("Start") and not st.session_state.running:
+        st.session_state.session = TelemetrySession()
         st.session_state.running = True
-        st.session_state.data = None
         st.rerun()
 
 with col2:
-    if st.button("Stop"):
-        st.session_state.stop_event.set()
-
-if st.session_state.running:
-    with st.spinner("Collecting telemetry... Press Stop to finish."):
-        st.session_state.data = start_f123_lap_telemetry(st.session_state.stop_event, ip_address=ip_address, port=port)
+    if st.button("Stop") and st.session_state.running:
+        data = st.session_state.session.stop()
+        st.session_state.data = data
         st.session_state.running = False
         st.rerun()
 
-if st.session_state.data is not None:
+if st.session_state.running:
+    st.info("Collecting telemetry... Press Stop to finish.")
+
+if "data" in st.session_state and st.session_state.data is not None:
     fig = plot_telemetry(st.session_state.data)
     st.pyplot(fig)
